ac62057ef8e9657af83fdd2e246a3b27bd9adb7d
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-06-11 01:56:45 +0300

    Add project files.

---
+++ Pages/Orders.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Data;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Pages
+{
+    public class OrdersModel : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders = new List<PizzaOrder>();
+        private readonly ApplicationDbContext _context;
+
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+        }
+    }
+}
