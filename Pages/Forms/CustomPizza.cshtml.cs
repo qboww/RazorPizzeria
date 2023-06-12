53c6400203eb96f90b88305a0d3e56cc4bb32d41
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-06-12 14:54:10 +0300

    custompizza and chekout ui rework

--- Pages/Forms/CustomPizza.cshtml.cs
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -18,15 +18,16 @@ namespace RazorPizzeria.Pages.Forms
         public IActionResult OnPost()
         {
             PizzaPrice = Pizza.BasePrice;
+            int DoughPrice = 200;
 
-            if (Pizza.TomatoSauce) PizzaPrice += 1;
-            if (Pizza.Cheese) PizzaPrice += 1;
-            if (Pizza.Peperoni) PizzaPrice += 1;
-            if (Pizza.Mushroom) PizzaPrice += 1;
-            if (Pizza.Tuna) PizzaPrice += 1;
-            if (Pizza.Pineapple) PizzaPrice += 10;
-            if (Pizza.Ham) PizzaPrice += 1;
-            if (Pizza.Beef) PizzaPrice += 1;
+            if (Pizza.TomatoSauce) PizzaPrice = DoughPrice += 10;
+            if (Pizza.Cheese) PizzaPrice = DoughPrice += 15;
+            if (Pizza.Peperoni) PizzaPrice = DoughPrice += 20;
+            if (Pizza.Mushroom) PizzaPrice = DoughPrice += 15;
+            if (Pizza.Tuna) PizzaPrice = DoughPrice += 25;
+            if (Pizza.Pineapple) PizzaPrice = DoughPrice += 30;
+            if (Pizza.Ham) PizzaPrice = DoughPrice += 20;
+            if (Pizza.Beef) PizzaPrice = DoughPrice += 25;
 
             return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
